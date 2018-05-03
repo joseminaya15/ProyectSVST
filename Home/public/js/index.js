7ea3bd24c9f9065cb640ff4b5aa5bbd34d0bd3bd
joseminaya15 <jose.minayac15@gmail.com>
2018-05-03 06:55:48 -0500

    segunda parte

--- Home/public/js/index.js
+++ Home/public/js/index.js
@@ -1,1 +1,15 @@
-﻿
+﻿$('#principal .owl-carousel').owlCarousel({
+    lazyLoad: true,
+    animateOut: 'fadeOut',
+    animateIn: 'fadeIn',
+    responsive: {
+        0: {
+            items: 1
+        }
+    },
+    navigation: false,
+    nav: false,
+    loop: true,
+    autoplay: true,
+    autoplayTimeout: 6000
+});
